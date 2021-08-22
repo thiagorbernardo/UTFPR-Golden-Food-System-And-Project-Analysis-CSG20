1badb4c358b0d8e4293bbee0685c8d7f2a53a73f
Thiago Ramos <thiagorbernardo@hotmail.com>
2021-08-21 22:08:25 -0300

    feat: forms components

---
+++ src/components/Forms/index.tsx
@@ -0,0 +1,27 @@
+import { Button, TextField } from "@material-ui/core";
+import React from "react";
+
+import FormsStyle from '../../styles/Forms.module.css'
+
+type TextInputProps = {
+  id: string,
+  value: any,
+  label: string,
+  onChange: Function
+  error: boolean
+  helperText?: string
+}
+
+export const TextInput: React.FC<TextInputProps> = ({id, value, label, onChange, error, helperText}) => {
+  return (<TextField
+    required
+    error={error}
+    id={id}
+    label={label}
+    // defaultValue={name || ""}
+    value={value}
+    onChange={e => onChange(e.target.value)}
+    helperText={error ? helperText || "Você precisa preencher esse valor.": ""}
+    variant="filled"
+  />)
+}
